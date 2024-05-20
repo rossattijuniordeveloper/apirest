650d96151da9fc41c9242c68ac7a069f62cc0f2d
rossattijuniordeveloper <rossattijuniordeveloper@gmail.com>
2024-05-20 16:32:28 -0300

    Listando todas as Skills

--- app/src/main/java/br/com/parapar/app/AppApplication.java
+++ app/src/main/java/br/com/parapar/app/AppApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class AppApplication {
 
-	public static void main(String[] args) {
-		System.out.println("aplicação rodando!");
+	public static void main(String[] args) {		
 		SpringApplication.run(AppApplication.class, args);
 	}
 
---
+++ app/src/main/java/br/com/parapar/app/api/skills/controllers/SkillRestController.java
@@ -0,0 +1,51 @@
+package br.com.parapar.app.api.skills.controllers;
+
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.parapar.app.api.skills.dtos.SkillResponse;
+import br.com.parapar.app.api.skills.mappers.SkillMapper;
+import br.com.parapar.app.core.repositories.SkillRepository;
+import lombok.RequiredArgsConstructor;
+
+//@Controller
+//@ResponseBody
+// substitui-se as anotações acima por @RestController
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/skills")
+public class SkillRestController {
+    
+    private final SkillMapper skillMapper;
+    private final SkillRepository skillRepository;
+
+
+    /* uma forma de retorno 
+    @GetMapping    
+    public ResponseEntity<List<SkillResponse>> findAll(){
+        var skills = skillRepository.findAll()
+        .stream()
+        .map(skillMapper::toSkillResponse)        
+        .toList();
+        return ResponseEntity.ok().body(skills);
+    }
+    */
+    // outra forma de retorno sem ResponseEntity na declaração do metodo e...
+    // sem criação da variavel skills e inclusão da anotação @ResponseBody no Nivel da Classe 
+    @GetMapping    
+//    @ResponseBody
+    public List<SkillResponse> findAll(){
+        return skillRepository.findAll()
+        .stream()
+        .map(skillMapper::toSkillResponse)        
+        .toList();        
+    }
+
+
+}
