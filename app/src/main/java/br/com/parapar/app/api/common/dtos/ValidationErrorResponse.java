10aa0a8ada418ff9ffb84aa235c25cbb5fe50acf
rossattijuniordeveloper <rossattijuniordeveloper@gmail.com>
2024-05-29 16:43:32 -0300

    Tratando Exceção MethodArgumentNotValidException

---
+++ app/src/main/java/br/com/parapar/app/api/common/dtos/ValidationErrorResponse.java
@@ -0,0 +1,22 @@
+package br.com.parapar.app.api.common.dtos;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Map;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ValidationErrorResponse {
+    private String message;
+
+    @Builder.Default
+    private LocalDateTime timestamp  = LocalDateTime.now();
+
+    private Map<String, List<String>> errors;
+}
--- app/src/main/java/br/com/parapar/app/api/common/handlers/ApiExceptionHandler.java
+++ app/src/main/java/br/com/parapar/app/api/common/handlers/ApiExceptionHandler.java
@@ -1,15 +1,20 @@
 package br.com.parapar.app.api.common.handlers;
 
+import java.util.stream.Collectors;
+
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import br.com.parapar.app.api.common.dtos.ErrorResponse;
+import br.com.parapar.app.api.common.dtos.ValidationErrorResponse;
 import br.com.parapar.app.core.exceptions.ModelNotFoundException;
 
 @RestControllerAdvice
 public class ApiExceptionHandler{    
+
     @ResponseStatus(code = HttpStatus.NOT_FOUND)
     @ExceptionHandler(ModelNotFoundException.class)
     public ErrorResponse handleModelNotFoundException( ModelNotFoundException exception){
@@ -17,4 +22,25 @@ public class ApiExceptionHandler{
         .message( exception.getLocalizedMessage() )
         .build();
     }
+
+    @ResponseStatus(code = HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ValidationErrorResponse handleMethodArgumentNotValidException(
+        MethodArgumentNotValidException exception
+        ){
+            var errors = exception.getBindingResult().getFieldErrors()
+            .stream()
+            .collect(Collectors.groupingBy(
+                fieldError -> fieldError.getField(),
+                Collectors.mapping(
+                    fieldError -> fieldError.getDefaultMessage(),
+                    Collectors.toList() 
+                )
+            ));
+
+            return ValidationErrorResponse.builder()
+            .message("Validation error")
+            .errors(errors)
+            .build();
+        }
 }
